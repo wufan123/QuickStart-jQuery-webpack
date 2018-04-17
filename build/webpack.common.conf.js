b4511c56d784fc9996d1ab477e12821565adca0c
wufan123 <357114638@qq.com>
2018-04-17 14:24:50 +0800

    添加html-withimg插件

--- build/webpack.common.conf.js
+++ build/webpack.common.conf.js
@@ -7,73 +7,85 @@ const webpack = require('webpack')
 const isPro = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  entry: utils.getEntries(),
-  output: {
-    path: config.build.assetsRoot,
-    publicPath: isPro ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
-    filename: '[name].js'
-  },
-  resolve: {
-    alias: {
-      jquery: 'jquery/dist/jquery.min.js'
+    entry: utils.getEntries(),
+    output: {
+        path: config.build.assetsRoot,
+        publicPath: isPro ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
+        filename: '[name].js'
     },
-    modules: ['src', 'node_modules']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        include: [path.join(__dirname, '..', 'src')],
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [
-          path.join(projectRoot, 'src')
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        query: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        query: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      }
+    resolve: {
+        alias: {
+            jquery: 'jquery/dist/jquery.min.js'
+        },
+        modules: ['src', 'node_modules']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                include: [path.join(__dirname, '..', 'src')],
+                options: {
+                    formatter: require('eslint-friendly-formatter')
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [
+                    path.join(projectRoot, 'src')
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-withimg-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            eslint: {
+                configFile: path.join(__dirname, '.eslintrc.js'),
+                formatter: require('eslint-friendly-formatter'),
+                useEslintrc: false
+            }
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new webpack.DefinePlugin({
+            isDevelopMode: !isPro
+        })
     ]
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      eslint: {
-        configFile: path.join(__dirname, '.eslintrc.js'),
-        formatter: require('eslint-friendly-formatter'),
-        useEslintrc: false
-      }
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-    new webpack.DefinePlugin({
-      isDevelopMode: !isPro
-    })
-  ]
 }
