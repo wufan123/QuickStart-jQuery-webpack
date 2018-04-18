276ef2783559699b961f9cfb4a928c9d79f7f2ab
wufan123 <357114638@qq.com>
2018-04-18 10:20:54 +0800

    ui实现

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,2 @@
-require('./../../public/main.css')
-console.log('~_~ jQuery can use directly ~_~')
-console.log(jQuery)
-console.log('add your code here ....')
-
-require('./../../public/main.js')
+import '../../public/main.css'
+import './index.less'
